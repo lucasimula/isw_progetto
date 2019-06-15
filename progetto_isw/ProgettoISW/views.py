0ba9d568868bd03106661ca8f1b1a88e50302cd6
lucasimula <lucasimula5@gmail.com>
2019-06-15 14:42:39 +0200

    Creato db + varue modifiche

--- progetto_isw/ProgettoISW/views.py
+++ progetto_isw/ProgettoISW/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
+
+def login(request):
+    return render(request)
+
+
+def home(request):
+    return render(request, "home.html", {'hotel': Hotel.objects.all()})
+
+
+def homeAlbergatore(request):
+    #albergatore
+    elencoPrenotazioni = []
+
+    for p in Prenotazione.objects.all():
+        elencoPrenotazioni.append(p)
+
+
+    return render(request, "homeAlbergatore.html", {'prenotazioni': elencoPrenotazioni})
+
+
