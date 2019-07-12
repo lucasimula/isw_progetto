27651ecdd6aaaa58913fda42f5a93ec6b3ad3f75
piccu <piccumartina@outlook.it>
2019-07-12 20:40:04 +0200

    inizioRicerca

--- progetto_isw/progetto_isw/urls.py
+++ progetto_isw/progetto_isw/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from progetto_isw.ProgettoISW import views
+from ProgettoISW import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
