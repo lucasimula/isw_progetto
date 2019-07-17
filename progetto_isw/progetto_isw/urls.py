fd1e89416daf29047b70fcb53f17767471dd3caa
AlessiaPisu <alessia.pisu96@gmail.com>
2019-07-17 19:02:29 +0200

    pagina Principale

--- progetto_isw/progetto_isw/urls.py
+++ progetto_isw/progetto_isw/urls.py
@@ -14,13 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 from ProgettoISW import views
-from django.urls import re_path
-
 
 urlpatterns = [
-    re_path(r'^$', views.cercaB), #imposta la pagina principale
+    re_path(r'^$', views.cercaB),
     path('admin/', admin.site.urls),
     path('home/', views.home),
     path('login/', views.login),
